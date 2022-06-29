2c91c3983e87f718ae1a3cc14c097f9b06472781
CuriosityBard <curiositybard@gmail.com>
2022-06-29 08:53:38 -0700

    Make buttons add values to screen

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+const calculator = {
+    currentInput: "",
+    dom: {
+        screen: document.querySelector('#screen-text'),
+    }
+}
+
 // basic arithmetic functions 
 function add(a, b) {
     return a + b;
@@ -24,4 +31,38 @@ function operate(a, b, op) {
         case '+':
             return add(a, b);
     }
+}
+
+// to take values from the buttons and add them to the screen
+function appendValue(val) {
+    let appendThis;
+    switch(val) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7: 
+        case 8:
+        case 9: 
+        case 0:
+            switch (calculator.currentInput.slice(calculator.currentInput.length - 1)) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    appendThis = " " + val.toString(); 
+                    break;
+                default:
+                    appendThis = val.toString();
+            }
+            break;
+        default:
+            appendThis = " " + val.toString(); 
+    }
+    calculator.currentInput += appendThis;
+
+    // display on screen: 
+    calculator.dom.screen.textContent = calculator.currentInput;
 }
