d25eee098b0414788f0a5f8b364a29d19d336f08
CuriosityBard <curiositybard@gmail.com>
2022-06-29 07:34:24 -0700

    Function to take operator and perform arithmetic

--- scripts.js
+++ scripts.js
@@ -12,3 +12,16 @@ function divide(a, b) {
     return a / b;
 }
 
+// to handle values from the calculator
+function operate(a, b, op) {
+    switch(op) {
+        case '/': 
+            return divide(a, b);
+        case '*':
+            return multiply(a, b);
+        case '-':
+            return subtract(a, b);
+        case '+':
+            return add(a, b);
+    }
+}
